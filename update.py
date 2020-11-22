9b601f564eb0acf3f56a818a1b0419a786200728
xiehengjian <846188037@qq.com>
2020-11-22 16:16:24 +0800

    update

--- update.py
+++ update.py
@@ -1,12 +1,10 @@
 from flask import Flask 
-from git import Repo
 import os
 app=Flask(__name__)
 
 @app.route('/update/blog',methods=["POST"])
 def update():
-    r = Repo("/Document/")
-    r.remote().pull()
+    os.system("cd /Document && git fetch --all && git reset --hard origin/master && git pull")
     return "更新完成"
 if __name__ == "__main__":
     app.run(debug=True,host="0.0.0.0",port=1200)
