cade1ca836def21aee35cca42fff24a747169611
xiehengjian <846188037@qq.com>
2020-11-22 14:53:57 +0800

    update

--- update.py
+++ update.py
@@ -7,6 +7,5 @@ app=Flask(__name__)
 def update():
     r = Repo("/Document/")
     r.remote().pull()
-    os.system("cd /Document/ && npm run build")
 if __name__ == "__main__":
     app.run(debug=True,host="0.0.0.0",port=1200)
